f384afe69d2705962f6899331285c154823da276
Basil K Y <basilky145@gmail.com>
2018-07-15 01:37:43 +0530

    added keys retrival

---
+++ getkeys.php
@@ -0,0 +1,25 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', 'r', 'secure');
+//$mysqli = new mysqli('localhost', 'mreuser1_mits', 'mits@123','mreuser1_healthvault');
+if ($mysqli->connect_errno) {
+    echo "Sorry, this website is experiencing problems.";
+    echo "Error: Failed to make a MySQL connection, here is why: \n";
+    echo "Errno: " . $mysqli->connect_errno . "\n";
+    echo "Error: " . $mysqli->connect_error . "\n";
+    exit;
+}
+
+$sql="select json_keys(passwords) as result from secret where username='basil'";
+$result=mysqli_query($mysqli,$sql);
+if($result==false)
+{
+    echo("Error description: " . mysqli_error($mysqli));
+}
+else 
+{
+    foreach($result as $row)
+    {
+    echo $row['result'];
+    }
+}
+?>
