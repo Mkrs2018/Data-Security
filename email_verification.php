1eadf32af2068b9b8c9069f8a2622c8fab364eae
Basil K Y <basilky145@gmail.com>
2018-07-15 12:18:18 +0530

    all working

--- email_verification.php
+++ email_verification.php
@@ -11,34 +11,56 @@ if ($mysqli->connect_errno) {
 if(!empty($_GET['code']) && isset($_GET['code']))
 {
 $code=$_GET['code'];
-$sql=mysqli_query($mysqli,"SELECT * FROM unverified_users WHERE activationcode='$code'");
-$num=mysqli_fetch_array($sql);
+$sql=$mysqli->prepare("SELECT * FROM unverified_users WHERE activationcode=?");
+$sql->bind_param("s",$code);
+$sql->execute();
+$sql->store_result();
+
+$num=$sql->num_rows;
+echo $num;
 //var_dump($code);
 if($num>0)
 {
 $st=0;
-$result=mysqli_query($mysqli,"SELECT * FROM unverified_users WHERE activationcode='$code' and status='$st'");
+$result=$mysqli->prepare("SELECT * FROM unverified_users WHERE activationcode=? and status=?");
+$result->bind_param("ss",$code,$st);
 //var_dump($result);
-$result4=mysqli_fetch_array($result);   
+$result->execute();
+$result->store_result();
+var_dump($result);
+$result4=$result->num_rows;   
 if($result4>0) 
  {
 $st=1;
-$result=mysqli_query($mysqli,"SELECT email FROM unverified_users WHERE activationcode='$code'");
+$result=$mysqli->prepare("SELECT email FROM unverified_users WHERE activationcode=?");
+$result->bind_param("s",$code);
+$result->execute();
+$result->store_result();
+
 foreach($result as $row)
 {
+
     // $dir="/home/mreuser1/public_html/myhealthvault.tk/documents/".$row['email'];
    //  	mkdir($dir,0777,true);
 	$msg="Your account is activated."; 
-    $result1=mysqli_query($mysqli,"UPDATE unverified_users SET status='$st' WHERE activationcode='$code'");
-    $sql2="select email,password from unverified_users where activationcode='$code'";
-    $result5=mysqli_query($mysqli,$sql2);
-    $array4 = mysqli_fetch_assoc($result5);
-    echo $array4['email'];
-    $email5=$array4['email'];
-    echo $array4['password'];
-    $password5=$array4['password'];
-    $sql6="insert into users (email,password) values ('$email5','$password5')";
-    $result6=mysqli_query($mysqli,$sql6);
+    $result1=$mysqli->prepare("UPDATE unverified_users SET status=? WHERE activationcode=?");
+    $result1->bind_param("ss",$st,$code);
+       $result1->execute();
+    echo("Error description: " . mysqli_error($mysqli));
+
+    $sql2=$mysqli->prepare("select email,password from unverified_users where activationcode=?");
+    $sql2->bind_param("s",$code);
+    $sql2->execute();
+     echo("Error description: " . mysqli_error($mysqli));
+    $sql2->store_result();
+    $sql2->bind_result($email5, $password5);
+    $sql2->fetch(); 
+    echo $email5;
+    echo $password5;
+    $sql6=$mysqli->prepare("insert into users (email,password) values (?,?)");
+    $sql6->bind_param("ss",$email5,$password5);
+    $sql6->execute();
+    echo("Error description: " . mysqli_error($mysqli));
 
 }
 }
